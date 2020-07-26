21dbbb8c45c27cdcd3ad8bbbbc2356cb629fa0c1
Stewart Granger Flores <stewart.grngrf@gmail.com>
2020-07-25 21:38:14 -0400

    Se modifica la letra :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,11 @@
 <style>
 @import url("https://fonts.googleapis.com/css?family=Nunito&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Lato&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Sora&display=swap");
 * {
   margin: 0;
   padding: 0;
-  font-family: "Lato";
+  font-family: "Sora";
   /* Remueve el fondo azul cuando clickeas algo en la pagina  https://stackoverflow.com/a/45050338/12533883*/
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   /* Remueve el fondo azul cuando clickeas algo en la pagina  https://stackoverflow.com/a/45050338/12533883*/
